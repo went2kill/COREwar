59541364539ff7e78b4ba78a545495760c841209
Yaroslav Robotko <yrobotko@e3r2p4.unit.ua>
2017-09-26 20:52:02 +0300

    ?

--- CorewaR/cmake-build-debug/CMakeFiles/asm_parsing.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for asm_parsing.
-# This may be replaced when dependencies are built.
--- CorewaR/test.s
+++
@@ -1,8 +0,0 @@
-.comment "just a basic living prog"
-.name "zor0k"
-		
-l2:	sti	r1,%:live,%0
-	and	r1,%0,r1
-live:	live	%1
-	zjmp	%:live
-
